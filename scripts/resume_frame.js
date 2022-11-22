fa9b207f11c6bb4ab3948b0d742d41859edb39de
Riley Spires <24spiresr@mydacc.org>
2022-11-22 09:48:52 -0500

    WIP resume page

---
+++ scripts/resume_frame.js
@@ -0,0 +1,38 @@
+const name = $("#name");
+const work = $("#work");
+const edu = $("#edu");
+const ext = $("#ext");
+const hob = $("#hob");
+const ref = $("#ref");
+
+name.on("click", show);
+work.on("click", show);
+edu.on("click", show);
+ext.on("click", show);
+hob.on("click", show);
+ref.on("click", show);
+
+$(document).ready(() => {
+	alert("Click each header for more information!");
+});
+
+function show() {
+	$(this).css("flex-direction", "column");
+	$(this).children("ul, ol").children().children("ul, ol").show();
+	$(this).children("ul, ol").show("slow");
+	$(this).animate({ height: "40%" }, "slow");
+	$(this).off();
+	$(this).on("click", hide);
+}
+
+function hide() {
+	$(this).animate({ height: "19.79%" }, "slow");
+	$(this)
+		.children("ul, ol")
+		.hide("slow", () => {
+			$(this).css("flex-direction", "row");
+			$(this).children("ul, ol").children().children("ul, ol").hide();
+		});
+	$(this).off();
+	$(this).on("click", show);
+}
